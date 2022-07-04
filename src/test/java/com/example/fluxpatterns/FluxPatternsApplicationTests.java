46c4f05ea4208986eb0d042c1336ae84f708d2c5
sahak1an <sahakianis@yahoo.com>
2022-07-04 10:44:06 +0400

    init

---
+++ src/test/java/com/example/fluxpatterns/FluxPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
